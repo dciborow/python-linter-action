3c21b65d6a2a2ee4b9d922e2a99742a062f08b81
Daniel Ciborowski <dciborow@microsoft.com>
2021-09-07 01:12:36 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,6 @@ if [[ -n "${GITHUB_WORKSPACE}" ]]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
-export REVIEWDOG_VERSION=v0.13.0
-
-echo "[action-pylint] Installing reviewdog..."
-wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /tmp "${REVIEWDOG_VERSION}"
-
 if [[ "$(which pylint)" == "" ]]; then
   echo "[action-pylint] Installing pylint package..."
   python -m pip install --upgrade pylint
@@ -21,10 +14,7 @@ pylint --version
 
 echo "[action-pylint] Checking python code with the pylint linter and reviewdog..."
 exit_val="0"
-pylint --rcfile ${INPUT_PYLINT_RC} -s n ${INPUT_WORKDIR} 2>&1
-
-echo "[action-pylint] Clean up reviewdog..."
-rm /tmp/reviewdog
+pylint --rcfile ${INPUT_PYLINT_RC} -s n ${INPUT_WORKDIR}
 
 if [[ "${exit_val}" -ne '0' ]]; then
   exit 1
